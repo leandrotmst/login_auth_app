813874e2912bc92b550b2be9866e8134ff15454a
leandro <leandrotomasetto@gmail.com>
2025-07-11 00:27:22 -0300

    Iniciando projeto

--- src/login_auth_app/app.py
+++ src/login_auth_app/app.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template
+# Flask
+# https://flask.palletsprojects.com/en/latest/
+
+# Flask-SQLAlchemy
+# https://docs.sqlalchemy.org/en/latest/
+
+# SQLAlchemy
+# https://docs.sqlalchemy.org/en/latest/
+
+# Flask-Login
+# https://flask-login.readthedocs.io/en/latest/
+
+# Flask Migrate
+# https://flask-migrate.readthedocs.io/en/latest/
+
+# Flask e banco de dados
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+
+# Real Python
+# https://realpython.com/
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('login.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("login.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    index()
